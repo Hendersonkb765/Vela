37aba25688d6dc6bd16aa4719db82218279ed953
Gustavorfatec476 <167939364+Gustavorfatec476@users.noreply.github.com>
2024-08-02 14:15:27 -0300

    FIX: Tailwind.config.js e route 'Resources'
    Reconfigurei o Tailwind que não foi salvo e criei novamente a rota "Resources" que leva para a página que apresenta os componentes Figma.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,32 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                neutral: {
+                  DEFAULT: '#1E1E1E', // Base Black
+                  100: '#F0EFED', // Soft Gray
+                  200: '#D8D8D8', // Light Gray
+                  300: '#737373', // Medium Gray
+                  400: '#3C3C3C', // Dark Gray
+                  500: '#374046', // Slate Gray
+                  600: '#1E1E1E', // Base Black
+                  700: '#161616', // Darker Black
+                },
+                primary: {
+                    DEFAULT: '#057EE8', // Standard Primary
+                    100: '#B6DCFF', // Light Primary
+                    200: '#0A53AB', // Medium Primary
+                    300: '#012340', // Dark Primary
+                    400: '#057EE8', // Standard Primary
+                },
+                danger: ['#DC3545'],
+                warning: ['#FFC107'],
+                success: ['#28A745'],
+                info: ['#17A2B8'],
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                body: ['Poppins'],
+                headers: ['Open Sans'],
             },
         },
     },
