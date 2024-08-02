dae0fc9811023cdd1916ec47553efebc1a8355f5
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-02 18:19:28 -0300

    feat: Cria o metodo RegisteredPresident, que registra o presidente, e a osc da qual ele pertece.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
-use Mockery\Expectation;
+use Illuminate\Support\Facades\DB;
+use App\Models\Osc;
 
 class RegisteredUserController extends Controller
 {
@@ -41,7 +42,9 @@ class RegisteredUserController extends Controller
             'position' => 'required',
         ]);
 
-        
+        if($request->position == 'Presidente'){
+            $createOsc = true;
+        }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -58,5 +61,42 @@ class RegisteredUserController extends Controller
         Auth::login($user);
 
         return redirect(route('osc', absolute: false));
+    }
+    public function registeredPresident(Request $request) : RedirectResponse{
+        
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'password' => 'required|confirmed|min:8',
+            'birthday' => 'required|date|before:today',
+            'sex' =>'required',
+        ]);
+
+        DB::beginTransaction();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'birthday' => $request->birthday,
+            'sex' => $request->sex,
+            'position' => $request->position,
+        ]);
+
+        $osc = Osc::create([
+            'name' => $request->name,
+            'presidents_name' => $request->name,
+            'foundation_date' => $request->foundation_date,
+        ]);
+
+        DB::commit();
+
+        //DB::rollBack();
+        
+
+
+
+        return redirect()->route('dashboard');
     }
 }
