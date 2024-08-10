fd1c9f6a4056ff56fe93cc051495bc0c4026231a
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-10 00:11:36 -0300

    chore: Adiciona nullable nas colunas

--- database/migrations/2024_08_02_203844_create_oscs_table.php
+++ database/migrations/2024_08_02_203844_create_oscs_table.php
@@ -17,15 +17,15 @@ return new class extends Migration
             $table->string('CNPJ')->unique();
             $table->string('institutional_email',);
             $table->string('phone_number');
-            $table->string('company_name');
-            $table->string('fantasy_name');
+            $table->string('company_name')->nullable();
+            $table->string('fantasy_name')->nullable();
             $table->string('presidents_name');
             $table->date('foundation_date');
             $table->string('banner_url')->nullable();
             $table->string('img_url')->nullable();
-            $table->string('legal_nature');
-            $table->string('statute_url');
-            $table->string('cnae_main');
+            $table->string('legal_nature')->nullable();
+            $table->string('statute_url')->nullable();
+            $table->string('cnae_main')->nullable();
             $table->foreignId('user_id')->constrained();
             
             $table->timestamps();
@@ -37,7 +37,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('osc_cnae', function (Blueprint $table) {
+        Schema::create('osc_cnaes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('osc_id')->constrained();
             $table->foreignId('cnae_id')->constrained();
@@ -61,7 +61,7 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
-        Schema::create('osc_type_performance', function (Blueprint $table) {
+        Schema::create('osc_type_performances', function (Blueprint $table) {
             $table->id();
             $table->foreignId('osc_id')->constrained();
             $table->foreignId('type_performance_id')->constrained();
