9fb7277f9f3f2e44f388263e4a0d3a870ed889a7
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-10 00:58:52 -0300

    --

--- database/migrations/2024_08_10_032749_create_oscs_users_table.php
+++ database/migrations/2024_08_10_032749_create_oscs_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('oscs_users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('osc_id')->constrained();
-            $table->foreignId('osc_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
