cfba6df6a918aa1bfd3fabfa9ef9ff099505f5e3
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-21 13:00:08 -0300

    feat: Cria os relacionamentos da tabela axis.

---
+++ app/Models/Axis.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Axis extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'description'
+    ];
+
+    public function osc(){
+        return $this->belongsToMany(Osc::class);
+    }
+
+    public function level(){
+        return $this->hasMany(Level::class);
+    }
+
+}
