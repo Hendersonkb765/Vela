572de323cdc6fcd0e4da647246cb92ee1c9895a0
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-30 19:29:09 -0300

    refactor: Trocando o nome das variaveis peding e completed.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\AxisOsc;
 use App\Models\Level;
 use Illuminate\Http\Request;
 use App\Models\Osc;
+use App\Models\TaskOrder;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB; // Add this line
@@ -28,12 +29,14 @@ class DashboardController extends Controller
         $tasks = Level::with(['task','task.step'])->where('id', $level->id)->first();
         $arrayTasks = ['axis'=>$axis->name,'completed'=>['total'=>0],'pending'=>['total'=>0],'tasks_max'=>0,'requirements_failed'=>0];
 
+        
         foreach ($tasks['task'] as $task) {
-            $peding = 0;
-            $completed = 0;
+
+            $numberPeding = 0;
+            $numberCompleted = 0;
             $step = [];
             
-            $taskNew = ['id'=> $task->id,'title'=>$task->title,'status'=>$task->status,'step'=>[],''];
+            $taskNew = ['id'=> $task->id,'title'=>$task->title,'status'=>$task->status,'step'=>[],'order_number'=>$task->order->order_number];
             foreach ($task['step'] as $step) {
 
                 foreach ($step->requirement as $requirement) {
@@ -41,30 +44,28 @@ class DashboardController extends Controller
                         $arrayTasks['requirements_failed']++;
                     }
                 }
+
                 if($step->status == 'completed'){
-                    $completed++;
+                    $numberCompleted++;
                 }else{
-                    $peding++;
+                    $numberPeding++;
                 }
                 $step = ['id'=>$step->id,'title'=>$step->title,'pending'=>0,'completed'=>0];
             }
-            $step['pending'] = $peding;
-            $step['completed'] = $completed;
+            $step['pending'] = $numberPeding;
+            $step['completed'] = $numberCompleted; 
             $taskNew['step'] = $step;
             $task->status == 'completed'?
             array_push($arrayTasks['completed'],$taskNew):
             array_push($arrayTasks['pending'],$taskNew);
-
         }
         //array_push($arrayTasks['completed'],['total'=> Level::where('id',$currentLevel)->first()->task->where('status','completed')->count()]);
         $arrayTasks['completed']['total'] = Level::where('id',$currentLevel)->first()->task->where('status','completed')->count();
         $arrayTasks['pending']['total'] = Level::where('id',$currentLevel)->first()->task->where('status','pending')->count();
         $arrayTasks['tasks_completed'] = Level::where('id',$currentLevel)->first()->task->where('status','completed')->count();
         $arrayTasks['tasks_max'] = Level::where('id',$currentLevel)->first()->task->count();
-
-        $requirements = $tasks;
-        //dd($requirements);
-        //dd($arrayTasks);
+        
+        
         return Inertia::render('Dashboard',[
             'user' => $user,
             'osc' => [
