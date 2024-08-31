058705700ca3b4d933a49d8d1956d9fd5f353c96
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-08-30 21:09:10 -0300

    fix: Adiciona o cargo do usuario.

--- resources/js/Layouts/VelaSocialLayout.jsx
+++ resources/js/Layouts/VelaSocialLayout.jsx
@@ -3,10 +3,10 @@ import NavBar from '@/FigmaComponents/NavMenu/NavBar';
 import SideMenu from '@/FigmaComponents/NavMenu/SideMenu';
 import { Link } from '@inertiajs/react';
 
-export default function VelaSocialLayout({ children, imgUrl, userName}) {
+export default function VelaSocialLayout({ children, imgUrl, userName,role}) {
     return (
         <div className='flex h-screen overflow-hidden'>
-            <NavBar className="pr-8" imgUrl={imgUrl} userName={userName} />
+            <NavBar className="pr-8" imgUrl={imgUrl} userName={userName} role={role} />
             <SideMenu />
             <section className='flex-1 flex flex-col bg-neutralcolors-100'>
                 {children}
