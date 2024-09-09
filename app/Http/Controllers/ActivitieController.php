6217ea355be00a8ee672c99edc1bdc834b084ea0
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-09-09 19:44:52 -0300

    Refatorar: Adicionar controlador de atividades e atualizar método de criação de atividades

--- app/Http/Controllers/ActivitieController.php
+++ app/Http/Controllers/ActivitieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activitie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,15 +11,34 @@ class ActivitieController extends Controller
     
     public function index(){
         //lista de atividades
+    
         return view('activitie.index');
     }
     public function create(){
         //formulario de cadastro de atividades
-        $user = Auth::user();
-        return view('activitie.create');
+        return view('activitie.create'); 
     }
-    public function store(){
+    public function store(Request $request){
 
+        try{
+            Auth::loginUsingId(14);
+            Activitie::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'start_date' => $request->start_date,
+                'end_date' => $request->end_date,
+                'status' => $request->status,
+                'user_id' => Auth::user()->id,
+                'osc_id' => Auth::user()->osc->first()->id
+            ]);
+    
+            return response()->json(['status'=> 200,'message' => 'Atividade cadastrada com sucesso!']);
+           
+        }
+        catch(\Exception $e){
+            return response()->json(['status'=> 500,'message' => 'Erro ao cadastrar atividade!']);
+        }
+       
 
     }
     public function update(){
