7610dc21dd0517f31427215f9c83b645c4dfffa5
Gustavorfatec476 <167939364+Gustavorfatec476@users.noreply.github.com>
2024-09-25 22:41:51 -0300

    (Hotfiz):Role

--- resources/js/Layouts/VelaSocialLayout.jsx
+++ resources/js/Layouts/VelaSocialLayout.jsx
@@ -4,7 +4,7 @@ import SideMenu from '@/FigmaComponents/NavMenu/SideMenu';
 import { Link, usePage } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 
-export default function VelaSocialLayout({ children}) {
+export default function VelaSocialLayout({children}) {
     const imgurl = "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90byUyMGRvJTIwcGVyZmlsfGVufDB8fDB8fHww"
     const { auth } = usePage().props;
     const { user } = auth;
@@ -26,7 +26,7 @@ export default function VelaSocialLayout({ children}) {
     return (
 
         <div className='flex overflow-x-hidden'>
-            <NavBar className="pr-8" imgUrl={imgurl} userName={user.name} role={user.role} email={user.email} />
+            <NavBar className="pr-8" imgUrl={imgurl} userName={user.name} role={user.roleInOrganization} email={user.email} />
             <SideMenu  />
             <section className='flex-1 flex flex-col bg-neutralcolors-100 dark:bg-gray-950 py-14 lg:pb-0'>
                 {children}
