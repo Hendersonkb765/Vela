e6043fef00d6769b59075906d916cc891fb4211b
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-09-27 12:39:07 -0300

    feat: Adiciona modelo Activity com relacionamentos e atributos preenchíveis

---
+++ app/Models/Activity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'hour_start',
+        'hour_end',
+        'date',
+        'audience',
+        'send_by',
+        'description',
+        'img_url',
+        'thumbnail_photos_url',
+        'photos_url',
+    ];
+
+    // muitos para um
+    public function osc(): BelongsTo
+    {
+        return $this->belongsTo(Osc::class);
+    }
+    public function address(){
+        return $this->morphMany(Address::class, 'addressable');
+    }
+}
