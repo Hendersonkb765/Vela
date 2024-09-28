b2e4f59fd3ae1aaa8ff4b33a3ce71f26d8c1d33e
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-09-28 04:00:59 -0300

    Refatorar ActivityController para lidar com filtragem e atualização de atividades
    Este commit refatora o ActivityController para adicionar funcionalidades de filtragem de atividades por nome e atualização de detalhes da atividade. Também adiciona tratamento de erros para recuperação e atualização de atividades. As mudanças incluem:
    
    Adicionar um novo método filterByName para filtrar atividades pelo título
    Modificar o método store para incluir validação para URLs de miniaturas e fotos
    Adicionar um novo método update para atualizar detalhes da atividade
    Adicionar um novo método destroy para deletar atividades
    Essas mudanças melhoram a funcionalidade e a manutenibilidade do ActivityController.

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,19 +6,44 @@ use App\Models\activity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use App\Models\Address;
 
 class ActivityController extends Controller
 {
     
     public function index(){
         //lista de atividades
-        $activities = Auth::user()->osc->first()->activities;
-        return Inertia::render('VelaSocialLab/TaskHub/TaskHub',[
-            'activities' => $activities
-        ]);   
+        try{
+            $activities = Auth::user()->osc->first()->activities;
+    
+            return Inertia::render('VelaSocialLab/TaskHub/TaskHub',[
+                'activities' => $activities
+            ]);   
+        }
+        catch(\Exception $e){
+            return response()->json(['status'=> 500,'message' => 'Erro ao buscar atividades!']);
+        }
+        
     }
-    public function create(){
-        //formulario de cadastro de atividades
+    public function filterByName($title){
+        //detalhes da atividade
+       
+        try{
+            if(!empty($title)){
+        
+                $activities = Auth::user()->osc->first()->activities();
+                $activitiesFilter =$activities->where('title','like',$title . '%')->get();
+                return response()->json(['status'=> 200,'activities' => $activitiesFilter]);
+            }
+        }
+        catch(\Exception $e){
+            return response()->json(['status'=> 500,'message' => 'Erro ao buscar atividades!']);
+        }
+       
+        
+    }
+    public function edit($Id){
+      
         
     }
     public function store(Request $request){
@@ -30,8 +55,9 @@ class ActivityController extends Controller
             'activityDate'=> 'required|date',
             'activityHourStart' => 'required|date_format:H:i|before:activityHourEnd',
             'activityHourEnd' => 'required|date_format:H:i|after:activityHourStart',
-            //'hour_start' => 'required|date_format:H:i|before:hour_end',
-            //'hour_end' => 'required|date_format:H:i|after:hour_start',
+            'activityThumbnailPhotosUrl' => 'required'|'url',
+            'activityPhotosUrl' => 'required'|'url',
+         
         ]);
         try{
             
@@ -39,8 +65,8 @@ class ActivityController extends Controller
                 'title' => $request->activityTitle,
                 'description' => $request->activityDescription,
                 'date' => $request->activityDate,
-                'hour_start' => '00:00', 
-                'hour_end' => '00:00',
+                'hour_start' => $request->activityHourStart, 
+                'hour_end' => $request->activityHourEnd,
                 'status' => $request->activityStatus,
                 'audience' => 333,
                 'img_url' => 'https://via.placeholder.com/150',
@@ -60,10 +86,35 @@ class ActivityController extends Controller
        
 
     }
-    public function update(){
-
+    public function update(Request $request,$id){
+        try{
+            $activity = Activity::find($id);
+            $activity->update([
+                'title' => $request->activityTitle,
+                'description' => $request->activityDescription,
+                'date' => $request->activityDate,
+                'hour_start' => $request->activityHourStart, 
+                'hour_end' => $request->activityHourEnd,
+                'status' => $request->activityStatus,
+                'audience' => $request->activityAudience,
+                'img_url' => 'https://via.placeholder.com/150',
+                'thumbnail_photos_url' => $request->activityThumbnailPhotosUrl,
+                'photos_url' => $request->activityPhotosUrl,
+            ]);
+            return response()->json(['status'=> 200,'message' => 'Atividade atualizada com sucesso!']);
+        }
+        catch(\Exception $e){
+            return response()->json(['status'=> 500,'message' => 'Erro ao atualizar atividade!']);
+        }
     }
-    public function destroy(){
-
+    public function destroy($id){
+        try{
+            $activity = Activity::find($id);
+            $activity->delete();
+            return response()->json(['status'=> 200,'message' => 'Atividade deletada com sucesso!']);
+        }
+        catch(\Exception $e){
+            return response()->json(['status'=> 500,'message' => 'Erro ao deletar atividade!']);
+        }
     }
 }
