f0b0f6682e465a9d14728622d9b642012f329f2d
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-09-28 19:03:00 -0300

    feat: Adiciona exemplos imagens e vídeos do Google Drive no formulário
    Adiciona campos adicionais no modelo GoogleDriveFile para suportar imagens e vídeos do Google Drive no formulário.

--- resources/views/Formulario.blade.php
+++ resources/views/Formulario.blade.php
@@ -3,3 +3,7 @@
     <input type="file" name="database">
     <input type="submit" value="Enviar">
 </form>
+<!-- Teste de imagem 
+<img src="https://drive.google.com/thumbnail?id=1CYjhl6Y0FFQthou8OmyUpq50gJDhqQfK&sz=w1000" alt="Imagem do Google Drive">
+<iframe src="https://drive.google.com/file/d/1jxSWEtbhv5iEaS1qIeBuzsqftydcPVJYBNtYdfv8pHQ/preview" width="640" height="480"></iframe>
+    -->
