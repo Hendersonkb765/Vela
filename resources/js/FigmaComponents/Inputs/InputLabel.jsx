3c777dbd4f3fc010912fc6e789251c2727e2a620
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-09-30 13:50:52 -0300

    Atualiza o .gitignore.

--- resources/js/FigmaComponents/Inputs/InputLabel.jsx
+++
@@ -1,7 +0,0 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (
-        <label {...props} className={`block font-medium text-sm text-neutralcolors-700 dark:text-neutralcolors-200` + className}>
-            {value ? value : children}
-        </label>
-    );
-}
--- resources/views/Formulario.blade.php
+++
@@ -1,9 +0,0 @@
-<form action="{{route('formulario')}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="database">
-    <input type="submit" value="Enviar">
-</form>
-<!-- Teste de imagem 
-<img src="https://drive.google.com/thumbnail?id=1CYjhl6Y0FFQthou8OmyUpq50gJDhqQfK&sz=w1000" alt="Imagem do Google Drive">
-<iframe src="https://drive.google.com/file/d/1jxSWEtbhv5iEaS1qIeBuzsqftydcPVJYBNtYdfv8pHQ/preview" width="640" height="480"></iframe>
-    -->
