a94687266759962bb1a10700bc5236602154c4d4
Gustavorfatec476 <167939364+Gustavorfatec476@users.noreply.github.com>
2024-10-02 20:54:53 -0300

    (FEAT): ActivityUploadResponsive
    Melhorei a versão mobile desse upload

--- resources/js/Pages/VelaSocialLab/ActivityHub/Components/ActivityUpload.jsx
+++ resources/js/Pages/VelaSocialLab/ActivityHub/Components/ActivityUpload.jsx
@@ -20,12 +20,12 @@ export default function ActivityUpload({ }){
         <>
             <div
                 onClick={openModal}
-                className="w-3/4 sm:w-1/3 p-2 rounded-lg border-dashed border-2 flex items-center gap-4 cursor-pointer bg-primary-200/25 hover:bg-primary-200/10 dark:bg-primary-300/25 hover:dark:bg-primary-200/10  text-white hover:scale-105 transition-all"
+                className="w-4/5 sm:w-1/3 p-2 rounded-lg border-dashed border-2 flex items-center gap-4 cursor-pointer bg-primary-200/25 hover:bg-primary-200/10 dark:bg-primary-300/25 hover:dark:bg-primary-200/10  text-white hover:scale-105 transition-all"
             >
                 <IoAddCircleOutline className="w-32 h-32 "/>
                 <div className="flex flex-col">
-                    <h2 className="font-headers font-bold text-lg ">Registrar Uma atividade</h2>
-                    <p>
+                    <h2 className="font-headers font-bold text-base sm:text-lg ">Registrar Uma atividade</h2>
+                    <p className="text-sm sm:text-base">
                         Registre aqui as atividades da sua organização
                         para montar sua linha do tempo
                     </p>
