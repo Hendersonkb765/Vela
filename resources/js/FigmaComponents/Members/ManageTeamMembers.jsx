c2f5f56089a0c9cfbce392992d6e0c77ddef69ff
Gustavorfatec476 <167939364+Gustavorfatec476@users.noreply.github.com>
2024-10-08 12:41:55 -0300

    (FIX) AddMember

--- resources/js/FigmaComponents/Members/ManageTeamMembers.jsx
+++ resources/js/FigmaComponents/Members/ManageTeamMembers.jsx
@@ -9,24 +9,25 @@ import { GoPaperAirplane, GoPersonAdd, GoX } from 'react-icons/go';
 import MembersList from './MembersList';
 
 const ManageTeamMembers = ({ isOpen, onClose, children }) => {
-    const initialMembers = [
-        { id: 1, name: 'Adedayo Bello', email: 'adedayo.bello@mail.com', status: 'MEMBRO', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3sqbhhfev_e_DCEWHWsZbyYh8UYSigVh8mQ&s'},
-        { id: 2, name: 'Courtney Henry', email: 'courtney.henry@mail.com', status: 'PENDENTE', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5zYaLainah5BClZST5tXxNk0u7_yJcD0KQ&s6bASBOZ8e43Ug+Jgea67mhed5rXJL7LvFum2bOUTypMyieoSsidChcVI8hRPXGEZWLXMFi04lZj54NSXELiq6s14YN/MLFi+rPnF2WJmKVIHcG7mEFim0NyxuSnGvAncvFi1rR0exG6yv6neL269f6oarY3gMF4jn+ysWIXjiEskiYYFcnXtvktX4Bcf8AzH4LFixwQSnI0ZgFUEE1XHzRl7buawa6rFiBIJsQ175w0OqR4swHvALFiWx0VQuos7zfEfNHirAc3nB9J+6xYgNmZZMOflII8yNPjC9vXS5ruYXqxcKfYf2sweYB+CEuzxWLEJyBX1Q4Q8Ho4aOH381EWU95qOPQNgnzJhYsXWNSN6d49rw9ncyiAN4y8jzVlstrqjIztzAgHu6aHmCfqsWIXCMuw45ZQ0mD3GLG8q6uLKbNTpORo9p2mrnH7IHEMQ7SoXNGVsBrG+6xoho+viSsWLoxSejs0m1sBc0b1pXqx3Rx3+CxYjEx2za7oEND43GPEcP31UAdxWLFzDjuJdf4YtmrWdAlrGiePecTH/5+CvxcsWLyfL/5Gej46+CNSVGaixYpRxqXg9FHXZCxYiRgOSsWLFph/9k=' },
-        { id: 3, name: 'Blessing Olasile', email: 'blessing.olasile@mail.com', status: 'EXPIRADO', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeeoNVMemefzDNwKGRJjQ_Wk6uXW1rg0k2Tw&s' },
-    ];
-
     const { data, setData, post, processing, errors } = useForm({
         Invitemail: '',
     });
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        post('invitation.send', {
+        alert("batata")
+        post(route('invitation.send'), {
             onSuccess: () => {
                 setData('Invitemail', '');
             },
         });
     };
+
+    const initialMembers = [
+        { id: 1, name: 'Adedayo Bello', email: 'adedayo.bello@mail.com', status: 'MEMBRO', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3sqbhhfev_e_DCEWHWsZbyYh8UYSigVh8mQ&s'},
+        { id: 2, name: 'Courtney Henry', email: 'courtney.henry@mail.com', status: 'PENDENTE', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5zYaLainah5BClZST5tXxNk0u7_yJcD0KQ&s6bASBOZ8e43Ug+Jgea67mhed5rXJL7LvFum2bOUTypMyieoSsidChcVI8hRPXGEZWLXMFi04lZj54NSXELiq6s14YN/MLFi+rPnF2WJmKVIHcG7mEFim0NyxuSnGvAncvFi1rR0exG6yv6neL269f6oarY3gMF4jn+ysWIXjiEskiYYFcnXtvktX4Bcf8AzH4LFixwQSnI0ZgFUEE1XHzRl7buawa6rFiBIJsQ175w0OqR4swHvALFiWx0VQuos7zfEfNHirAc3nB9J+6xYgNmZZMOflII8yNPjC9vXS5ruYXqxcKfYf2sweYB+CEuzxWLEJyBX1Q4Q8Ho4aOH381EWU95qOPQNgnzJhYsXWNSN6d49rw9ncyiAN4y8jzVlstrqjIztzAgHu6aHmCfqsWIXCMuw45ZQ0mD3GLG8q6uLKbNTpORo9p2mrnH7IHEMQ7SoXNGVsBrG+6xoho+viSsWLoxSejs0m1sBc0b1pXqx3Rx3+CxYjEx2za7oEND43GPEcP31UAdxWLFzDjuJdf4YtmrWdAlrGiePecTH/5+CvxcsWLyfL/5Gej46+CNSVGaixYpRxqXg9FHXZCxYiRgOSsWLFph/9k=' },
+        { id: 3, name: 'Blessing Olasile', email: 'blessing.olasile@mail.com', status: 'EXPIRADO', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeeoNVMemefzDNwKGRJjQ_Wk6uXW1rg0k2Tw&s' },
+    ];
 
     const AddMemberByMail = () => {
         return (
@@ -39,13 +40,13 @@ const ManageTeamMembers = ({ isOpen, onClose, children }) => {
                 </h2>
                 <form onSubmit={handleSubmit} className="flex flex-row sm:items-center space-x-2">
                     <div>
-                        <InputLabel htmlFor="Invitemail" value="" />
+                        <InputLabel htmlFor="Invitemail" />
                         <TextInput
                             id="Invitemail"
                             type="email"
                             name="Invitemail"
                             value={data.Invitemail}
-                            className="h-12 block w-64 sm:w-full px-4"
+                            className="h-12 block w-full px-4"
                             placeholder="E-mail do destinatário"
                             onChange={(e) => setData('Invitemail', e.target.value)}
                             required
@@ -53,12 +54,10 @@ const ManageTeamMembers = ({ isOpen, onClose, children }) => {
                         <InputError message={errors.Invitemail} className="mt-2 text-danger" />
                     </div>
 
-                    <div>
-                        <PrimaryButton type="submit" center={true} className="w-12 sm:w-auto !min-w-fit h-12" disabled={processing}>
-                            <span className="sm:block hidden">{processing ? 'Enviando...' : 'Enviar Convite'}</span>
-                            <GoPaperAirplane className="w-4 h-4"/>
-                        </PrimaryButton>
-                    </div>
+                    <PrimaryButton type="submit" center={true} className="w-12 sm:w-auto !min-w-fit h-12" disabled={processing}>
+                        <span className="sm:block hidden">{processing ? 'Enviando...' : 'Enviar Convite'}</span>
+                        <GoPaperAirplane className="w-4 h-4"/>
+                    </PrimaryButton>
                 </form>
             </>
         );
