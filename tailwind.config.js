5ef0734f3688dec0fe618c2874892098dc2e6d8d
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-10-14 14:58:44 -0300

    Refatora configurações de fonte e imagem de fundo

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,9 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-
+                headers: ['Poppins'],
+                body: ['Open Sans'],
+            },
             screens: {
                 'fullhd': '1920px', // Breakpoint para telas de 1920px ou maiores
             },
@@ -44,31 +46,11 @@ export default {
                 success: '#28A745',
                 info: '#17A2B8',
             },
-            fontFamily: {
-                headers: ['Poppins'],
-                body: ['Open Sans'],
-            },
-            
             keyframes: {
                 fade: {
                   '0%, 100%': { opacity: '0.2' }, // Opacidade mínima
                   '50%': { opacity: '1' }, // Opacidade máxima
                 },
-              },
-              // Associando a keyframe com a animação
-              animation: {
-                fade: 'fade 2s ease-in-out infinite', // 2s de duração, ease-in-out, repetição infinita
-            },
-
-
-
-            backgroundImage: {
-                'custom-gradient': 'linear-gradient(to right top, #057ee8, #3786e8, #4f8de8, #6295e7, #729de7, #6da9ee, #69b4f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1)',
-              },
-              animation: {
-                'gradient-move': 'gradient-move 3s ease infinite',
-              },
-              keyframes: {
                 'gradient-move': {
                   '0%': {
                     'background-position': '0% 50%',
@@ -80,35 +62,29 @@ export default {
                     'background-position': '0% 50%',
                   },
                 },
-              },
-              backgroundSize: {
+                fly: {
+                  '0%': { transform: 'translate(0, 0) rotate(0deg)' },
+                  '50%': { transform: 'translate(50px, -30px) rotate(10deg)' },
+                  '100%': { transform: 'translate(100px, 0) rotate(20deg)' },
+                },
+                enter: {
+                  '0%': { transform: 'scale(0)' }, // Opacidade mínima
+                  '50%': { transform: 'scale(1.1)' }, // Opacidade máxima
+                  '100%': { transform: 'scale(1)' },
+                },
+            },
+            animation: {
+                fade: 'fade 2s ease-in-out infinite', // 2s de duração, ease-in-out, repetição infinita
+                'gradient-move': 'gradient-move 3s ease infinite',
+                fly: 'fly 3s ease-in-out infinite',
+                enter: 'enter 0.4s ease-in-out forwards', // 2s de duração, ease-in-out, repetição infinita
+            },
+            backgroundImage: {
+                'custom-gradient': 'linear-gradient(to right top, #057ee8, #3786e8, #4f8de8, #6295e7, #729de7, #6da9ee, #69b4f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1)',
+            },
+            backgroundSize: {
                 '200%': '200%',
             },
-            
-            keyframes: {
-              fly: {
-                '0%': { transform: 'translate(0, 0) rotate(0deg)' },
-                '50%': { transform: 'translate(50px, -30px) rotate(10deg)' },
-                '100%': { transform: 'translate(100px, 0) rotate(20deg)' },
-              },
-            },
-            animation: {
-              fly: 'fly 3s ease-in-out infinite',
-            },
-
-
-            keyframes: {
-              enter: {
-                '0%': { transform: 'scale(0)' }, // Opacidade mínima
-                '50%': { transform: 'scale(1.1)' }, // Opacidade máxima
-                '100%': { transform: 'scale(1)' },
-              },
-            },
-            // Associando a keyframe com a animação
-            animation: {
-              enter: 'enter 0.4s ease-in-out forwards', // 2s de duração, ease-in-out, repetição infinita
-            },
-
         },
     },
 
